2a7482488d2297fd7fce883d12f93ef1ef72cab2
Farawyr <domenicocaliendo95@gmail.com>
2018-03-24 18:32:01 +0100

    - Aggiunta la Navigation Bar - Aggiunta Tab Bar - Aggiunta la fotocamera e il live preview nella ViewController - Ridimensionamento e Fill camera in base al dispositivo usato

--- Inspectron2.0/Inspectron2.0/ViewController.swift
+++ Inspectron2.0/Inspectron2.0/ViewController.swift
@@ -7,19 +7,58 @@
 //
 
 import UIKit
+import AVFoundation // Framework, cercare uso.
+
+
+
 
 class ViewController: UIViewController {
 
+    var captureSession: AVCaptureSession? //Avvia sessione fotocamera con tutti i dispositivi I/O Audio Video
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer?  //inizializzo due variabili da poter riutilizzare nel codice. Aiuta a renderizzare il cameraViewFinder nella View Controller
+    var inputCamera: AVCaptureInput?
+    
+    
+@IBOutlet weak var Camera: UIImageView! // Collegato a UIImageView
+        
+        
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        let captureDevice = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back) //Imposta il device di acquisizione
+        
+        do{
+        inputCamera = try AVCaptureDeviceInput(device: captureDevice!)
+        }catch{
+            print(error)
+        }
+        
+        
+        captureSession = AVCaptureSession() // Inizializza la variabile di CaptureSession (Coordina stream di coordinate e dati di immagine
+        captureSession?.addInput(inputCamera!)
+        
+        videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession!)
+        videoPreviewLayer?.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        
+        //videoPreviewLayer?.frame = self.Camera!.layer.bounds
+        self.Camera?.layer.addSublayer(self.videoPreviewLayer!)
+        self.videoPreviewLayer?.frame = UIScreen.main.bounds // quello che ci ha salvato la vita con la dimensione della fotocamera
+        
+        captureSession?.startRunning()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+   
+    
 
-
+    
 }
 
