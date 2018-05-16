5057cf9dc1e7901cbaee35f2bc96e9bdb7e4dfda
GennaroTheBugger7 <37736283+GennaroTheBugger7@users.noreply.github.com>
2018-05-16 10:50:24 +0200

    Icons + NoWarnings

--- Inspectron2.0/Inspectron2.0/AppDelegate.swift
+++ Inspectron2.0/Inspectron2.0/AppDelegate.swift
@@ -16,7 +16,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        
         //navbar background
         let navBackgroundImage = UIImage(named: "navbar-background")
         UINavigationBar.appearance().setBackgroundImage(navBackgroundImage, for: .default)
